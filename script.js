489536ed8c8e50c679475b6684abcdb9368beec1
Cody Fisher <fishercody23@gmail.com>
2024-07-24 20:57:48 -0600

    The form now works so that the is saved in local storage meaning that if the webpage is refresh or closed the data will still exist.

--- script.js
+++ script.js
@@ -16,13 +16,30 @@ function addBookToLibrary(author, title, pages, genre, read) {
     // do stuff here    
     const book = new Book(author, title, pages, genre, read)
 
-    myLibrary.push(book)
+    let data = JSON.parse(localStorage.getItem('myLibrary')) || [];
+
+    console.log("Data before push: ", data)
+
+    data.push(book);
+
+    console.log("Data after push: ", data)
+
+    localStorage.setItem('myLibrary', JSON.stringify(data));
+
+    //myLibrary.push(book)
     
+}
+
+function getData() {
+    return JSON.parse(localStorage.getItem('myLibrary')) || [];
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
     const form = document.getElementById('form');
+    const bookAdded = document.getElementById('bookAdded');
+
+    
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -31,11 +48,24 @@ document.addEventListener('DOMContentLoaded', () => {
         title = document.getElementById('title').value;
         pages = document.getElementById('pages').value;
         genre = document.getElementById('genre').value;
-        read = document.getElementById('read').value;
+        read = document.getElementById('read').checked;
 
         addBookToLibrary(author, title, pages, genre, read);
 
-        console.log(myLibrary)
+        bookAdded.innerText = 'Book Added'
+
+        
+
+        setTimeout(() => {
+            document.getElementById('author').value = '';
+            document.getElementById('title').value = '';
+            document.getElementById('pages').value = '';
+            document.getElementById('genre').value = '';
+            document.getElementById('read').value = '';
+            bookAdded.innerText = ''
+        }, 3000)
+
+        console.log(getData())
     })
 })
 
